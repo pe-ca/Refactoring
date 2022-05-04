9b70d1d69de37fed2f3048676f758e3e280cf67e
pe-ca <mr.gloser1@gmail.com>
2022-05-03 20:53:46 -0500

    se extrae la seccion encargada de la lectura de archivos para refactorizar en una clase separada

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const fs = require("fs"); // se importa el modulo fs para manejo de archivos
 
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json"); // se utiliza el metodo readFileSync que recibe como parametro el path del archivo a leer
-const explorers = JSON.parse(rawdata); // se usa json.ṕarse para convertir el objeto a json
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node"); // se usa el metodo filter para poder obtener solo los elementos que cumplen con la condicion 
