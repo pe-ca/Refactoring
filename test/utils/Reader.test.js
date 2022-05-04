219a7fa0c905d806550531d560df4bbf08ea6307
pe-ca <mr.gloser1@gmail.com>
2022-05-03 21:23:27 -0500

    definicion de clase reader con metodo estatico y creacion de primera prueba unitaria para reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,7 @@
+const reader = require("./../../lib/utils/Reader")
+
+describe("Pruebas unitarias de reader", () =>{
+    test("El resultado de la lectura no es undefined", () =>{
+        expect(reader.readJsonFile("./data/explorers.json")).not.toBeUndefined()
+    })
+})
