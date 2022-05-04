d9bdac8a36b0a83fddf2a4a2baee7a6cb5481b43
Jesús Perales <jesus@campa.com.mx>
2022-05-04 14:47:07 -0500

    se separan las funciones relacionadas a explorers y se agregan a su propia clase

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 
 
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node"); // se usa el metodo filter para poder obtener solo los elementos que cumplen con la condicion 
-console.log(explorersInNode.length) // imprime la longitud (cantidad de elementos) 
-
-// Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node"); // se usa el metodo filter para poder obtener solo los elementos que cumplen con la condicion 
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername); // map genera un nuevo arreglo a partir de los elementos que cumplen la condicion que se implementa
-console.log(usernamesInNode) // imprime el arreglo  generado a partir de map
-
-// DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
 // Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
 // Score: 3, Trick: FIZZ.
