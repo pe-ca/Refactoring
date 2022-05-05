0fc87202603bea19eb6bad6e7cc2e79c44e6ad69
Jesús Perales <jesus@campa.com.mx>
2022-05-05 15:25:35 -0500

    test caso fizz

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -5,4 +5,10 @@ describe("Fizzbuzz Service", () =>{
         result = typeof fbs.applyValidationInExplorer
         expect(result).toBe("function")
     })
+
+    test("Caso Fizz", ()=>{
+        const expl ={name: "Explorer1", score: 1}
+        result = fbs.applyValidationInExplorer(expl)
+        expect(result.trick).toBe(1)
+    })
 })
