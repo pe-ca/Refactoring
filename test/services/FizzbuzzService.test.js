587c8865e2bc009a5d57bd382cf7832a08529f29
pe-ca <mr.gloser1@gmail.com>
2022-05-08 14:08:53 -0500

    aplicadas correcciones de Linter

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,50 +1,50 @@
-const fbs = require("./../../lib/services/FizzbuzzService")
+const fbs = require("./../../lib/services/FizzbuzzService");
 
 describe("Fizzbuzz Service", () =>{
     test("Existe el metodo", ()=>{
-        result = typeof fbs.applyValidationInExplorer
-        expect(result).toBe("function")
-    })
+        const result = typeof fbs.applyValidationInExplorer;
+        expect(result).toBe("function");
+    });
 
     test("Caso objeto sin propiedad score", ()=>{
-        const expl ={name: "Explorer1"}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result).toBeUndefined()
-    })
+        const expl ={name: "Explorer1"};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result).toBeUndefined();
+    });
 
     test("Caso objeto sin propied trick", ()=>{
-        const expl ={name: "Explorer1", score: 1}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.hasOwnProperty("trick")).toBe(true)
-    })
+        const expl ={name: "Explorer1", score: 1};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.hasOwnProperty("trick")).toBe(true);
+    });
 
     test("Caso score no divisible entre 3", ()=>{
-        const expl ={name: "Explorer1", score: 1}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.trick).toBe(1)
-    })
+        const expl ={name: "Explorer1", score: 1};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.trick).toBe(1);
+    });
 
     test("Caso score divisible entre 3", ()=>{
-        const expl ={name: "Explorer3", score: 3}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.trick).toBe("Fizz")
-    })
+        const expl ={name: "Explorer3", score: 3};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.trick).toBe("Fizz");
+    });
 
     test("Caso score no divisible entre 5", ()=>{
-        const expl ={name: "Explorer4", score: 4}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.trick).toBe(4)
-    })
+        const expl ={name: "Explorer4", score: 4};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.trick).toBe(4);
+    });
 
     test("Caso score divisible entre 5", ()=>{
-        const expl ={name: "Explorer5", score: 5}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.trick).toBe("Buzz")
-    })
+        const expl ={name: "Explorer5", score: 5};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.trick).toBe("Buzz");
+    });
 
     test("Caso score divisible entre 3 y 5", ()=>{
-        const expl ={name: "Explorer15", score: 15}
-        result = fbs.applyValidationInExplorer(expl)
-        expect(result.trick).toBe("FizzBuzz")
-    })
-})
+        const expl ={name: "Explorer15", score: 15};
+        const result = fbs.applyValidationInExplorer(expl);
+        expect(result.trick).toBe("FizzBuzz");
+    });
+});
